d8fa293c2bbfd5e451f8c8f68bf93586217ad720
Oceanknight <2827280689zou@gmail.com>
2023-08-21 20:31:43 +0800

    feat: 开启边到边

--- app/src/main/java/com/oceanknight/mima/MainActivity.kt
+++ app/src/main/java/com/oceanknight/mima/MainActivity.kt
@@ -3,6 +3,8 @@ package com.oceanknight.mima
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.oceanknight.mima.ui.theme.MimaTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,6 +20,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 开启边到边
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             MimaTheme {
                 // A surface container using the 'background' color from the theme
@@ -33,10 +39,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
