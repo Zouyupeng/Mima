a5f7743eb2998a8adcedd4f33a872cc9d2207f1c
Oceanknight <2827280689zou@gmail.com>
2023-09-06 23:15:30 +0800

    🚧wip: 处理一下导航栏的label
    猫猫头抽到了！肉鸽15也打过了！把毛丫丫辛辛苦苦攒的金币霍霍到三百块了😂还是菜，只能打胡局

--- app/src/main/java/com/oceanknight/mima/ui/navigation/MimaNavBar.kt
+++ app/src/main/java/com/oceanknight/mima/ui/navigation/MimaNavBar.kt
@@ -1,15 +1,18 @@
 package com.oceanknight.mima.ui.navigation
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.NavGraph.Companion.findStartDestination
-import com.oceanknight.mima.ui.component.MimaIcon
 
 @Composable
 fun MimaNavBar(
@@ -38,10 +41,21 @@ fun MimaNavBar(
                 },
                 icon = {
                     if (currentNav == destination.name) {
-                        MimaIcon(destination.selectedIconId!!, label)
+                        Icon(
+                            modifier = Modifier.size(28.dp),
+                            painter = painterResource(id = destination.selectedIconId!!),
+                            contentDescription = label
+                        )
                     } else {
-                        MimaIcon(destination.unselectedIconId!!, label)
+                        Icon(
+                            modifier = Modifier.size(28.dp),
+                            painter = painterResource(id = destination.unselectedIconId!!),
+                            contentDescription = label
+                        )
                     }
+                },
+                label = {
+                    Text(text = label)
                 }
             )
         }
